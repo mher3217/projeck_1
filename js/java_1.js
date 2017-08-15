9c76bc50177cf05b4a83172695b0cbffec0b4540
mher3217 <mher-harutyunyan92@mail.ru>
2017-08-15 11:19:09 +0400

    Update java_1.js

--- js/java_1.js
+++ js/java_1.js
@@ -2,25 +2,6 @@
 function myFunction() {
     document.getElementById("m_1").classList.toggle("show");
 }
-/* myFunction toggles between adding and removing the show class, which is used to hide and show the dropdown content */
-function myFunction() {
-    document.getElementById("m_1").classList.toggle("show");
-}
-
-/*var myImage = document.getElementById("myPhoto");
-var imageArray = [photo_1, photo_2, photo_3, photo_4, photo_5, photo_6];
-var imageIndex = 0;
-function change_photo(){
-	myPhoto.setAttribute("src", imageArray [imageIndex]);
-	imageIndex ++;
-	if(imageIndex >= imageArray.length){
-		imageIndex = 0;
-	}
-}
-var intervalHandle = setInterval(change_photo, 2000);
-function slyder_stop(){
-	clearInterval(intervalHandle);
-}*/
 
 var index = 1;
 function plusIndex(){
@@ -40,5 +21,20 @@ function show_image(n){
 	for(var i = 0; i < x.length; ++i){
 		x[i].style.display = "none";
 	}
-		x[index - 1].style.display = "block";
+	x[index - 1].style.display = "block";
+}
+
+auto_change();
+function auto_change(){
+	var i;
+	var x = document.getElementsByClassName("slyder_fade");
+	for(var i = 0; i < x.length; ++i){
+		x[i].style.display = "none";
+	}
+	if(index > x.length){
+		index = 1;
+	}
+	x[index - 1].style.display = "block";	
+	index ++;
+	setTimeout(auto_change,2000);
 }
